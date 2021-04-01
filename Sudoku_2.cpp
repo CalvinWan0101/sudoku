1b363f6126a4162e137f7d47e9c00c8508855dd5
CalvinWan <s0610078@my.cmsh.cyc.edu.tw>
2021-04-01 15:07:22 +0800

    Update Sudoku_2.cpp

--- Sudoku_2.cpp
+++ Sudoku_2.cpp
@@ -1,60 +1,74 @@
 #include <iostream>
 using namespace std;
-int map[10][10], y, x, sy, sx;
+int sudoku[9][9];
 bool Test(int count)
 {
-    y = count / 9 + 1;
-    x = count % 9;
-    for (int i = 1; i <= 9; i++)
-        if (map[i][x] == map[y][x] && i != y)
-            return false;
-    for (int i = 1; i <= 9; i++)
-        if (map[y][i] == map[y][x] && i != x)
-            return false;
-    sy = (y - 1) / 3 * 3 + 1;
-    sx = (x - 1) / 3 * 3 + 1;
-    for (int i = sy; i < sy + 3; i++)
-        for (int j = sx; j < sx + 3; j++)
-            if (map[i][j] == map[y][x] && i != y && j != x)
-                return false;
-    return true;
+
+	int y = count / 9;
+	int x = count % 9;
+	//水平檢查
+	for (int i = 0; i < 9; ++i)
+		if (sudoku[y][i] == sudoku[y][x] && i != x)
+			return false;
+	//垂直檢查
+	for (int i = 0; i < 9; ++i)
+		if (sudoku[i][x] == sudoku[y][x] && i != y)
+			return false;
+	//小格檢查
+	//小格左上角那一格的x,y坐標為sx,sy
+	int sy = y / 3 * 3;
+	int sx = x / 3 * 3;
+	for (int i = sy; i < sy + 3; ++i)
+		for (int k = sx; k < sx + 3; ++k)
+			if (sudoku[i][k] == sudoku[y][x] && i != y && k != x)
+				return false;
+	//default
+	return true;
 }
-void function(int count)
+//count是格子的編號,從左上角開始左往右數,0號到80號
+void guess(int count)
 {
-    if (count == 82)
-    {
-        for (int i = 1; i <= 9; i++)
-        {
-            for (int j = 1; j <= 9; j++)
-                cout << map[i][j] << " ";
-            cout << endl;
-        }
-        cout << endl;
-        return;
-    }
-    y = count / 9 + 1;
-    x = count % 9;
-    if (x == 0)
-        x = 9, y--;
-    if (map[y][x] == 0)
-    {
-        for (int i = 1; i <= 9; i++)
-        {
-            map[y][x] = i;
-            if (Test(count))
-                function(count + 1);
-        }
-        map[y][x] = 0;
-    }
-    else
-        function(count + 1);
+	//因為格子最後一號只有到80,當count==81時表示已經把一個答案跑出來了
+	//此刻直接印出
+	if (count == 81)
+	{
+		for (int i = 0; i < 9; ++i)
+		{
+			for (int j = 0; j < 9; ++j)
+				cout << sudoku[i][j] << " ";
+			cout << endl;
+		}
+		return;
+	}
+	int y = count / 9;
+	int x = count % 9;
+	if (sudoku[y][x] == 0)
+	{
+		for (int i = 1; i <= 9; ++i)
+		{
+			//給這一格一個值,同時該格非0表示該格已經使用過了
+			sudoku[y][x] = i;
+			//測試這個值放在這一格是不是符合要求(丟進Test函數進行測試)
+			if (Test(count))
+			//通過的話就進入下一格
+				guess(count + 1);
+		}
+		//9個數字都嘗試完了可是不符合
+		//重置該點數值為0讓他回到上一層
+		sudoku[y][x] = 0;
+	}
+	//這一格有數字
+	else
+		guess(count + 1);
 }
 int main()
 {
-    for (int i = 1; i <= 9; i++)
-        for (int j = 1; j <= 9; j++)
-            cin >> map[i][j];
-    cout << endl;
-    function(1);
-    return 0;
+	for (int i = 0; i < 9; ++i)
+		for (int j = 0; j < 9; ++j)
+			cin >> sudoku[i][j];
+	cout << endl;
+	//第一格是0號，count代0進入開始跑
+	guess(0);
+	system("PAUSE");
+	return 0;
 }
